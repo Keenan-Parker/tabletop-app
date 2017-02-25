2f907aec825976aa1108eb512c8f02ab1df17794
Keenan Parker <keenantparker@hotmail.com>
2017-02-25 13:00:56 +1100

    postgres

--- db/migrate/20170224043417_drop_room_users.rb
+++
@@ -1,8 +0,0 @@
-class DropRoomUsers < ActiveRecord::Migration[5.0]
-  drop_table :room_users do |t|
-    t.integer :room_id
-    t.integer :player_id
-
-    t.timestamps
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170224044135) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "characters", force: :cascade do |t|
     t.string   "name"
     t.string   "race"
@@ -46,7 +49,7 @@ ActiveRecord::Schema.define(version: 20170224044135) do
     t.integer  "user_id"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
-    t.index ["user_id"], name: "index_characters_on_user_id"
+    t.index ["user_id"], name: "index_characters_on_user_id", using: :btree
   end
 
   create_table "items", force: :cascade do |t|
@@ -58,7 +61,7 @@ ActiveRecord::Schema.define(version: 20170224044135) do
     t.integer  "character_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.index ["character_id"], name: "index_items_on_character_id"
+    t.index ["character_id"], name: "index_items_on_character_id", using: :btree
   end
 
   create_table "room_characters", force: :cascade do |t|
@@ -75,7 +78,7 @@ ActiveRecord::Schema.define(version: 20170224044135) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.string   "gm_name"
-    t.index ["user_id"], name: "index_rooms_on_user_id"
+    t.index ["user_id"], name: "index_rooms_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -91,8 +94,8 @@ ActiveRecord::Schema.define(version: 20170224044135) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "visitors", force: :cascade do |t|
@@ -105,4 +108,7 @@ ActiveRecord::Schema.define(version: 20170224044135) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "characters", "users"
+  add_foreign_key "items", "characters"
+  add_foreign_key "rooms", "users"
 end
